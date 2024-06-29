0216884c3274a85ef5a356136d3bb1e709fab3a3
Per Christian Moen <per.christian.moen@nav.no>
2024-06-29 14:39:44 +0200

    Konverterer til Kotlin

--- src/main/kotlin/no/nav/pensjon/elsam/minibuss/nav_cons_elsam_np_tjenestepensjon/Mapper.kt
+++ src/main/kotlin/no/nav/pensjon/elsam/minibuss/nav_cons_elsam_np_tjenestepensjon/Mapper.kt
@@ -1,38 +1,51 @@
-package no.nav.pensjon.elsam.minibuss.nav_cons_elsam_np_tjenestepensjon;
+package no.nav.pensjon.elsam.minibuss.nav_cons_elsam_np_tjenestepensjon
+
+import nav_lib_cons_elsam_np.no.nav.lib.elsam.np.asbo.tjenestepensjon.ASBONpHarTjenestepensjonsforholdRequest
+import nav_lib_cons_elsam_np.no.nav.lib.elsam.np.asbo.tjenestepensjon.ASBONpHarTjenestepensjonsforholdResponse
+import nav_lib_cons_elsam_np.no.nav.lib.elsam.np.fault.FaultNpPersonIkkeFunnet
+import nav_lib_frg.no.nav.lib.frg.fault.FaultElementetFinnesIkke
+import nav_lib_frg.no.nav.lib.frg.gbo.GBOFinnTjenestepensjonsforholdRequest
+import nav_lib_frg.no.nav.lib.frg.gbo.GBOTjenestepensjon
+import javax.xml.datatype.DatatypeConfigurationException
+import javax.xml.datatype.DatatypeFactory
 
 
-import nav_lib_cons_elsam_np.no.nav.lib.elsam.np.asbo.tjenestepensjon.ASBONpHarTjenestepensjonsforholdRequest;
-import nav_lib_cons_elsam_np.no.nav.lib.elsam.np.asbo.tjenestepensjon.ASBONpHarTjenestepensjonsforholdResponse;
-import nav_lib_cons_elsam_np.no.nav.lib.elsam.np.fault.FaultNpPersonIkkeFunnet;
-import nav_lib_frg.no.nav.lib.frg.gbo.GBOTjenestepensjonForhold;
-
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import java.util.List;
-
-public class Mapper {
-    public static void ASBONpHarTjenestepensjonsRequestTOGBOFinnTjenestepensjonsforholdRequest(ASBONpHarTjenestepensjonsforholdRequest ASBONpHarTjenestepensjonsforholdRequest, nav_lib_frg.no.nav.lib.frg.gbo.GBOFinnTjenestepensjonsforholdRequest GBOFinnTjenestepensjonsforholdRequest) {
-        GBOFinnTjenestepensjonsforholdRequest.setFnr(ASBONpHarTjenestepensjonsforholdRequest.getFnr()); // move (executionOrder=1)
+object Mapper {
+    fun ASBONpHarTjenestepensjonsRequestTOGBOFinnTjenestepensjonsforholdRequest(
+        ASBONpHarTjenestepensjonsforholdRequest: ASBONpHarTjenestepensjonsforholdRequest,
+        GBOFinnTjenestepensjonsforholdRequest: GBOFinnTjenestepensjonsforholdRequest
+    ) {
+        GBOFinnTjenestepensjonsforholdRequest.fnr =
+            ASBONpHarTjenestepensjonsforholdRequest.fnr // move (executionOrder=1)
     }
 
-    public static void FaultElementetFinnesIkkeTOFaultNpPersonIkkeFunnet(nav_lib_frg.no.nav.lib.frg.fault.FaultElementetFinnesIkke FaultElementetFinnesIkke, FaultNpPersonIkkeFunnet FaultNpPersonIkkeFunnet) throws DatatypeConfigurationException {
-        FaultNpPersonIkkeFunnet.setErrorMessage(FaultElementetFinnesIkke.getErrorMessage()); // move (executionOrder=1)
-        FaultNpPersonIkkeFunnet.setErrorSource(FaultElementetFinnesIkke.getErrorSource()); // move (executionOrder=2)
-        FaultNpPersonIkkeFunnet.setErrorType(FaultElementetFinnesIkke.getErrorType()); // move (executionOrder=3)
-        FaultNpPersonIkkeFunnet.setDateTimeStamp(DatatypeFactory.newInstance().newXMLGregorianCalendar(FaultElementetFinnesIkke.getDateTimeStamp())); // move (executionOrder=4)
-        FaultNpPersonIkkeFunnet.setRootCause(FaultElementetFinnesIkke.getRootCause()); // move (executionOrder=5)
+    @Throws(DatatypeConfigurationException::class)
+    fun FaultElementetFinnesIkkeTOFaultNpPersonIkkeFunnet(
+        FaultElementetFinnesIkke: FaultElementetFinnesIkke, FaultNpPersonIkkeFunnet: FaultNpPersonIkkeFunnet
+    ) {
+        FaultNpPersonIkkeFunnet.errorMessage = FaultElementetFinnesIkke.errorMessage // move (executionOrder=1)
+        FaultNpPersonIkkeFunnet.errorSource = FaultElementetFinnesIkke.errorSource // move (executionOrder=2)
+        FaultNpPersonIkkeFunnet.errorType = FaultElementetFinnesIkke.errorType // move (executionOrder=3)
+        FaultNpPersonIkkeFunnet.dateTimeStamp = DatatypeFactory.newInstance()
+            .newXMLGregorianCalendar(FaultElementetFinnesIkke.dateTimeStamp) // move (executionOrder=4)
+        FaultNpPersonIkkeFunnet.rootCause = FaultElementetFinnesIkke.rootCause // move (executionOrder=5)
     }
 
-    public static void GBOTjenestepensjonTOASBONpHarTjenestepensjonsforholdResponse(nav_lib_frg.no.nav.lib.frg.gbo.GBOTjenestepensjon GBOTjenestepensjon, ASBONpHarTjenestepensjonsforholdResponse ASBONpHarTjenestepensjonsforholdResponse) {
-        List<GBOTjenestepensjonForhold> GBOTjenestepensjon_tjenestepensjonForholdene = GBOTjenestepensjon.getTjenestepensjonForholdene(); // custom.input.forEach (executionOrder=1)
-        boolean ASBONpHarTjenestepensjonsforholdResponse_harTjenestepensjonsforhold; // custom.output declaration (executionOrder=1)
+    fun GBOTjenestepensjonTOASBONpHarTjenestepensjonsforholdResponse(
+        GBOTjenestepensjon: GBOTjenestepensjon,
+        ASBONpHarTjenestepensjonsforholdResponse: ASBONpHarTjenestepensjonsforholdResponse
+    ) {
+        val GBOTjenestepensjon_tjenestepensjonForholdene =
+            GBOTjenestepensjon.tjenestepensjonForholdene // custom.input.forEach (executionOrder=1)
 
-        if (GBOTjenestepensjon_tjenestepensjonForholdene != null && !GBOTjenestepensjon_tjenestepensjonForholdene.isEmpty()) {
-            ASBONpHarTjenestepensjonsforholdResponse_harTjenestepensjonsforhold = true;
-        } else {
-            ASBONpHarTjenestepensjonsforholdResponse_harTjenestepensjonsforhold = false;
-        }
+        val ASBONpHarTjenestepensjonsforholdResponse_harTjenestepensjonsforhold =
+            if (GBOTjenestepensjon_tjenestepensjonForholdene != null && !GBOTjenestepensjon_tjenestepensjonForholdene.isEmpty()) {
+                true
+            } else {
+                false
+            } // custom.output declaration (executionOrder=1)
 
-        ASBONpHarTjenestepensjonsforholdResponse.setHarTjenestepensjonsforhold(ASBONpHarTjenestepensjonsforholdResponse_harTjenestepensjonsforhold); // custom.output assignment (executionOrder=1)
+        ASBONpHarTjenestepensjonsforholdResponse.harTjenestepensjonsforhold =
+            ASBONpHarTjenestepensjonsforholdResponse_harTjenestepensjonsforhold // custom.output assignment (executionOrder=1)
     }
 }
