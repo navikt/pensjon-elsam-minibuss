8ff9b6f3f07e3458f5b49858e6179aa5c350360b
Per Christian Moen <per.christian.moen@nav.no>
2024-06-29 14:45:53 +0200

    Rydder opp og gjør om til extension functions

--- src/main/kotlin/no/nav/pensjon/elsam/minibuss/nav_cons_elsam_np_tjenestepensjon/Mapper.kt
+++ src/main/kotlin/no/nav/pensjon/elsam/minibuss/nav_cons_elsam_np_tjenestepensjon/Mapper.kt
@@ -6,46 +6,26 @@ import nav_lib_cons_elsam_np.no.nav.lib.elsam.np.fault.FaultNpPersonIkkeFunnet
 import nav_lib_frg.no.nav.lib.frg.fault.FaultElementetFinnesIkke
 import nav_lib_frg.no.nav.lib.frg.gbo.GBOFinnTjenestepensjonsforholdRequest
 import nav_lib_frg.no.nav.lib.frg.gbo.GBOTjenestepensjon
-import javax.xml.datatype.DatatypeConfigurationException
 import javax.xml.datatype.DatatypeFactory
 
-
-object Mapper {
-    fun ASBONpHarTjenestepensjonsRequestTOGBOFinnTjenestepensjonsforholdRequest(
-        ASBONpHarTjenestepensjonsforholdRequest: ASBONpHarTjenestepensjonsforholdRequest,
-        GBOFinnTjenestepensjonsforholdRequest: GBOFinnTjenestepensjonsforholdRequest
-    ) {
-        GBOFinnTjenestepensjonsforholdRequest.fnr =
-            ASBONpHarTjenestepensjonsforholdRequest.fnr // move (executionOrder=1)
+// ASBONpHarTjenestepensjonsRequestTOGBOFinnTjenestepensjonsforholdRequest
+fun ASBONpHarTjenestepensjonsforholdRequest.toGBOFinnTjenestepensjonsforholdRequest() =
+    GBOFinnTjenestepensjonsforholdRequest().also {
+        it.fnr = fnr // move (executionOrder=1)
     }
 
-    @Throws(DatatypeConfigurationException::class)
-    fun FaultElementetFinnesIkkeTOFaultNpPersonIkkeFunnet(
-        FaultElementetFinnesIkke: FaultElementetFinnesIkke, FaultNpPersonIkkeFunnet: FaultNpPersonIkkeFunnet
-    ) {
-        FaultNpPersonIkkeFunnet.errorMessage = FaultElementetFinnesIkke.errorMessage // move (executionOrder=1)
-        FaultNpPersonIkkeFunnet.errorSource = FaultElementetFinnesIkke.errorSource // move (executionOrder=2)
-        FaultNpPersonIkkeFunnet.errorType = FaultElementetFinnesIkke.errorType // move (executionOrder=3)
-        FaultNpPersonIkkeFunnet.dateTimeStamp = DatatypeFactory.newInstance()
-            .newXMLGregorianCalendar(FaultElementetFinnesIkke.dateTimeStamp) // move (executionOrder=4)
-        FaultNpPersonIkkeFunnet.rootCause = FaultElementetFinnesIkke.rootCause // move (executionOrder=5)
+// FaultElementetFinnesIkkeTOFaultNpPersonIkkeFunnet
+fun FaultElementetFinnesIkke.toFaultNpPersonIkkeFunnet(): FaultNpPersonIkkeFunnet =
+    FaultNpPersonIkkeFunnet().also {
+        it.errorMessage = errorMessage // move (executionOrder=1)
+        it.errorSource = errorSource // move (executionOrder=2)
+        it.errorType = errorType // move (executionOrder=3)
+        it.dateTimeStamp = dateTimeStamp?.let { DatatypeFactory.newInstance().newXMLGregorianCalendar(it) } // move (executionOrder=4)
+        it.rootCause = rootCause // move (executionOrder=5)
     }
 
-    fun GBOTjenestepensjonTOASBONpHarTjenestepensjonsforholdResponse(
-        GBOTjenestepensjon: GBOTjenestepensjon,
-        ASBONpHarTjenestepensjonsforholdResponse: ASBONpHarTjenestepensjonsforholdResponse
-    ) {
-        val GBOTjenestepensjon_tjenestepensjonForholdene =
-            GBOTjenestepensjon.tjenestepensjonForholdene // custom.input.forEach (executionOrder=1)
-
-        val ASBONpHarTjenestepensjonsforholdResponse_harTjenestepensjonsforhold =
-            if (GBOTjenestepensjon_tjenestepensjonForholdene != null && !GBOTjenestepensjon_tjenestepensjonForholdene.isEmpty()) {
-                true
-            } else {
-                false
-            } // custom.output declaration (executionOrder=1)
-
-        ASBONpHarTjenestepensjonsforholdResponse.harTjenestepensjonsforhold =
-            ASBONpHarTjenestepensjonsforholdResponse_harTjenestepensjonsforhold // custom.output assignment (executionOrder=1)
+// GBOTjenestepensjonTOASBONpHarTjenestepensjonsforholdResponse
+fun GBOTjenestepensjon.toASBONpHarTjenestepensjonsforholdResponse(): ASBONpHarTjenestepensjonsforholdResponse =
+    ASBONpHarTjenestepensjonsforholdResponse().also {
+        it.harTjenestepensjonsforhold = tjenestepensjonForholdene.isNotEmpty() // custom.output assignment (executionOrder=1)
     }
-}
