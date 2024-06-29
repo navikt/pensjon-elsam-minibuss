87d4a9b328f3c18a2bbf071f26e5d0a2d77648fd
Per Christian Moen <per.christian.moen@nav.no>
2024-06-29 19:48:11 +0200

    Konverterer til Kotlin

--- src/main/kotlin/no/nav/pensjon/elsam/minibuss/nav_cons_elsam_tptilb_registreretpforholdV0_1/Mapper.kt
+++ src/main/kotlin/no/nav/pensjon/elsam/minibuss/nav_cons_elsam_tptilb_registreretpforholdV0_1/Mapper.kt
@@ -1,81 +1,122 @@
-package no.nav.pensjon.elsam.minibuss.nav_cons_elsam_tptilb_registreretpforholdV0_1;
+package no.nav.pensjon.elsam.minibuss.nav_cons_elsam_tptilb_registreretpforholdV0_1
 
-import jakarta.xml.bind.JAXBElement;
-import nav_cons_elsam_tptilb_registreretpforhold.no.nav.asbo.HentTPForholdListeRequestInt;
-import nav_cons_elsam_tptilb_registreretpforhold.no.nav.asbo.OpprettTPForholdRequestInt;
-import nav_cons_elsam_tptilb_registreretpforhold.no.nav.asbo.SlettTPForholdFinnTjenestepensjonsforholdRequestInt;
-import nav_cons_elsam_tptilb_registreretpforhold.no.nav.asbo.SlettTPForholdTjenestepensjonRequestInt;
-import nav_lib_frg.no.nav.lib.frg.gbo.GBOTjenestepensjonForhold;
-import no.nav.elsam.registreretpforhold.v0_1.FaultGenerisk;
-import no.nav.elsam.registreretpforhold.v0_1.FaultTjenestepensjonForholdIkkeFunnet;
-import no.nav.elsam.registreretpforhold.v0_1.HentTPForholdListeResp;
-import no.nav.elsam.registreretpforhold.v0_1.TPForhold;
+import nav_cons_elsam_tptilb_registreretpforhold.no.nav.asbo.HentTPForholdListeRequestInt
+import nav_cons_elsam_tptilb_registreretpforhold.no.nav.asbo.OpprettTPForholdRequestInt
+import nav_cons_elsam_tptilb_registreretpforhold.no.nav.asbo.SlettTPForholdFinnTjenestepensjonsforholdRequestInt
+import nav_cons_elsam_tptilb_registreretpforhold.no.nav.asbo.SlettTPForholdTjenestepensjonRequestInt
+import nav_lib_frg.no.nav.lib.frg.fault.FaultElementetErUgyldig
+import nav_lib_frg.no.nav.lib.frg.fault.FaultElementetFinnesIkke
+import nav_lib_frg.no.nav.lib.frg.fault.FaultTomDatoForanFomDato
+import nav_lib_frg.no.nav.lib.frg.gbo.GBOFinnTjenestepensjonsforholdRequest
+import nav_lib_frg.no.nav.lib.frg.gbo.GBOTjenestepensjon
+import nav_lib_frg.no.nav.lib.frg.gbo.GBOTjenestepensjonForhold
+import no.nav.elsam.registreretpforhold.v0_1.FaultGenerisk
+import no.nav.elsam.registreretpforhold.v0_1.FaultTjenestepensjonForholdIkkeFunnet
+import no.nav.elsam.registreretpforhold.v0_1.HentTPForholdListeResp
+import no.nav.elsam.registreretpforhold.v0_1.TPForhold
+import javax.xml.datatype.DatatypeConfigurationException
+import javax.xml.datatype.DatatypeFactory
 
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-
-
-public class Mapper {
-    public static void FaultElementetErUgyldigTOFaultTjenestepensjonForholdIkkeFunnet(nav_lib_frg.no.nav.lib.frg.fault.FaultElementetErUgyldig FaultElementetErUgyldig, FaultTjenestepensjonForholdIkkeFunnet FaultTjenestepensjonForholdIkkeFunnet) throws DatatypeConfigurationException {
-        FaultTjenestepensjonForholdIkkeFunnet.setErrorMessage(FaultElementetErUgyldig.getErrorMessage()); // move (executionOrder=1)
-        FaultTjenestepensjonForholdIkkeFunnet.setErrorSource(FaultElementetErUgyldig.getErrorSource()); // move (executionOrder=2)
-        FaultTjenestepensjonForholdIkkeFunnet.setRootCause(FaultElementetErUgyldig.getRootCause()); // move (executionOrder=3)
-        FaultTjenestepensjonForholdIkkeFunnet.setDateTimeStamp(DatatypeFactory.newInstance().newXMLGregorianCalendar(FaultElementetErUgyldig.getDateTimeStamp())); // move (executionOrder=4)
+object Mapper {
+    @Throws(DatatypeConfigurationException::class)
+    fun FaultElementetErUgyldigTOFaultTjenestepensjonForholdIkkeFunnet(
+        FaultElementetErUgyldig: FaultElementetErUgyldig,
+        FaultTjenestepensjonForholdIkkeFunnet: FaultTjenestepensjonForholdIkkeFunnet
+    ) {
+        FaultTjenestepensjonForholdIkkeFunnet.errorMessage =
+            FaultElementetErUgyldig.errorMessage // move (executionOrder=1)
+        FaultTjenestepensjonForholdIkkeFunnet.errorSource =
+            FaultElementetErUgyldig.errorSource // move (executionOrder=2)
+        FaultTjenestepensjonForholdIkkeFunnet.rootCause = FaultElementetErUgyldig.rootCause // move (executionOrder=3)
+        FaultTjenestepensjonForholdIkkeFunnet.dateTimeStamp = DatatypeFactory.newInstance()
+            .newXMLGregorianCalendar(FaultElementetErUgyldig.dateTimeStamp) // move (executionOrder=4)
     }
 
-    public static void FaultElementetFinnesIkkeTOFaultTjenestepensjonForholdIkkeFunnet(nav_lib_frg.no.nav.lib.frg.fault.FaultElementetFinnesIkke FaultElementetFinnesIkke, FaultTjenestepensjonForholdIkkeFunnet FaultTjenestepensjonForholdIkkeFunnet) throws DatatypeConfigurationException {
-        FaultTjenestepensjonForholdIkkeFunnet.setErrorMessage(FaultElementetFinnesIkke.getErrorMessage()); // move (executionOrder=1)
-        FaultTjenestepensjonForholdIkkeFunnet.setErrorSource(FaultElementetFinnesIkke.getErrorSource()); // move (executionOrder=2)
-        FaultTjenestepensjonForholdIkkeFunnet.setRootCause(FaultElementetFinnesIkke.getRootCause()); // move (executionOrder=3)
-        FaultTjenestepensjonForholdIkkeFunnet.setDateTimeStamp(DatatypeFactory.newInstance().newXMLGregorianCalendar(FaultElementetFinnesIkke.getDateTimeStamp())); // move (executionOrder=4)
+    @Throws(DatatypeConfigurationException::class)
+    fun FaultElementetFinnesIkkeTOFaultTjenestepensjonForholdIkkeFunnet(
+        FaultElementetFinnesIkke: FaultElementetFinnesIkke,
+        FaultTjenestepensjonForholdIkkeFunnet: FaultTjenestepensjonForholdIkkeFunnet
+    ) {
+        FaultTjenestepensjonForholdIkkeFunnet.errorMessage =
+            FaultElementetFinnesIkke.errorMessage // move (executionOrder=1)
+        FaultTjenestepensjonForholdIkkeFunnet.errorSource =
+            FaultElementetFinnesIkke.errorSource // move (executionOrder=2)
+        FaultTjenestepensjonForholdIkkeFunnet.rootCause = FaultElementetFinnesIkke.rootCause // move (executionOrder=3)
+        FaultTjenestepensjonForholdIkkeFunnet.dateTimeStamp = DatatypeFactory.newInstance()
+            .newXMLGregorianCalendar(FaultElementetFinnesIkke.dateTimeStamp) // move (executionOrder=4)
     }
 
-    public static void FaultTomDatoForanFomDatoTOFaultGenerisk(nav_lib_frg.no.nav.lib.frg.fault.FaultTomDatoForanFomDato FaultTomDatoForanFomDato, FaultGenerisk FaultGenerisk) {
-        FaultGenerisk.setErrorDescription(FaultTomDatoForanFomDato.getErrorMessage()); // move (executionOrder=1)
-        FaultGenerisk.setErrorCode("InternalError"); // set (executionOrder=2)
+    fun FaultTomDatoForanFomDatoTOFaultGenerisk(
+        FaultTomDatoForanFomDato: FaultTomDatoForanFomDato, FaultGenerisk: FaultGenerisk
+    ) {
+        FaultGenerisk.errorDescription = FaultTomDatoForanFomDato.errorMessage // move (executionOrder=1)
+        FaultGenerisk.errorCode = "InternalError" // set (executionOrder=2)
     }
 
-    public static void GBOTjenestepensjonForholdTOTPForhold(nav_lib_frg.no.nav.lib.frg.gbo.GBOTjenestepensjonForhold GBOTjenestepensjonForhold, TPForhold TPForhold) {
-        TPForhold.setTpnr(GBOTjenestepensjonForhold.getTpnr()); // move (executionOrder=1)
-        TPForhold.setTpnavn(GBOTjenestepensjonForhold.getNavn()); // move (executionOrder=2)
+    fun GBOTjenestepensjonForholdTOTPForhold(
+        GBOTjenestepensjonForhold: GBOTjenestepensjonForhold, TPForhold: TPForhold
+    ) {
+        TPForhold.tpnr = GBOTjenestepensjonForhold.tpnr // move (executionOrder=1)
+        TPForhold.tpnavn = GBOTjenestepensjonForhold.navn // move (executionOrder=2)
     }
 
-    public static void GBOTjenestepensjonTOHentTPForholdListeResp(nav_lib_frg.no.nav.lib.frg.gbo.GBOTjenestepensjon GBOTjenestepensjon, HentTPForholdListeResp HentTPForholdListeResponse) {
-        HentTPForholdListeResponse.getTjenestepensjonForholdene().addAll(GBOTjenestepensjon.getTjenestepensjonForholdene().stream().map(it -> {
-            TPForhold tpForhold = new TPForhold();
-            GBOTjenestepensjonForholdTOTPForhold(it, tpForhold);
-            return tpForhold;
-        }).toList()); // submap (executionOrder=1)
+    fun GBOTjenestepensjonTOHentTPForholdListeResp(
+        GBOTjenestepensjon: GBOTjenestepensjon, HentTPForholdListeResponse: HentTPForholdListeResp
+    ) {
+        HentTPForholdListeResponse.tjenestepensjonForholdene.addAll(
+            GBOTjenestepensjon.tjenestepensjonForholdene.stream().map { it: GBOTjenestepensjonForhold ->
+                val tpForhold = TPForhold()
+                GBOTjenestepensjonForholdTOTPForhold(it, tpForhold)
+                tpForhold
+            }.toList()
+        ) // submap (executionOrder=1)
     }
 
-    public static void HentTPForholdListeRequestIntTOGBOFinnTjenestepensjonsforholdRequest(HentTPForholdListeRequestInt HentTPForholdListeRequestInt, nav_lib_frg.no.nav.lib.frg.gbo.GBOFinnTjenestepensjonsforholdRequest GBOFinnTjenestepensjonsforholdRequest) {
-        GBOFinnTjenestepensjonsforholdRequest.setFnr(HentTPForholdListeRequestInt.getExtRequest().getFnr()); // move (executionOrder=1)
-        GBOFinnTjenestepensjonsforholdRequest.setHentSamhandlerInfo("true"); // set (executionOrder=2)
+    fun HentTPForholdListeRequestIntTOGBOFinnTjenestepensjonsforholdRequest(
+        HentTPForholdListeRequestInt: HentTPForholdListeRequestInt,
+        GBOFinnTjenestepensjonsforholdRequest: GBOFinnTjenestepensjonsforholdRequest
+    ) {
+        GBOFinnTjenestepensjonsforholdRequest.fnr =
+            HentTPForholdListeRequestInt.extRequest.fnr // move (executionOrder=1)
+        GBOFinnTjenestepensjonsforholdRequest.hentSamhandlerInfo = "true" // set (executionOrder=2)
     }
 
-    public static void OpprettTPForholdRequestIntTOGBOTjenestepensjon(OpprettTPForholdRequestInt OpprettTPForholdRequestInt, nav_lib_frg.no.nav.lib.frg.gbo.GBOTjenestepensjon GBOTjenestepensjon) {
-        GBOTjenestepensjon.setFnr(OpprettTPForholdRequestInt.getExtRequest().getFnr()); // move (executionOrder=1)
-        GBOTjenestepensjonForhold gboTjenestepensjonForhold = new GBOTjenestepensjonForhold();
-        OpprettTPForholdRequestIntTOGBOTjenestepensjonForhold(OpprettTPForholdRequestInt, gboTjenestepensjonForhold);
-        GBOTjenestepensjon.getTjenestepensjonForholdene().add(gboTjenestepensjonForhold); // submap (executionOrder=2)
+    fun OpprettTPForholdRequestIntTOGBOTjenestepensjon(
+        OpprettTPForholdRequestInt: OpprettTPForholdRequestInt, GBOTjenestepensjon: GBOTjenestepensjon
+    ) {
+        GBOTjenestepensjon.fnr = OpprettTPForholdRequestInt.extRequest.fnr // move (executionOrder=1)
+        val gboTjenestepensjonForhold = GBOTjenestepensjonForhold()
+        OpprettTPForholdRequestIntTOGBOTjenestepensjonForhold(OpprettTPForholdRequestInt, gboTjenestepensjonForhold)
+        GBOTjenestepensjon.tjenestepensjonForholdene.add(gboTjenestepensjonForhold) // submap (executionOrder=2)
     }
 
-    public static void OpprettTPForholdRequestIntTOGBOTjenestepensjonForhold(OpprettTPForholdRequestInt OpprettTPForholdRequestInt, nav_lib_frg.no.nav.lib.frg.gbo.GBOTjenestepensjonForhold GBOTjenestepensjonForhold) {
-        GBOTjenestepensjonForhold.setTssEksternId(OpprettTPForholdRequestInt.getEksternTSSId()); // move (executionOrder=1)
-        GBOTjenestepensjonForhold.setHarUtlandPensjon("false"); // set (executionOrder=2)
-        JAXBElement<Boolean> OpprettTPForholdRequestInt_extRequest_samtykke = OpprettTPForholdRequestInt.getExtRequest().getSamtykke(); // custom.input.forEach (executionOrder=3)
-        GBOTjenestepensjonForhold.setSamtykkeSimuleringKode((OpprettTPForholdRequestInt_extRequest_samtykke != null && OpprettTPForholdRequestInt_extRequest_samtykke.getValue() != null && OpprettTPForholdRequestInt_extRequest_samtykke.getValue()) ? "J" : "N"); // custom.output assignment (executionOrder=3)
-        GBOTjenestepensjonForhold.setHarSimulering("false"); // set (executionOrder=4)
+    fun OpprettTPForholdRequestIntTOGBOTjenestepensjonForhold(
+        OpprettTPForholdRequestInt: OpprettTPForholdRequestInt, GBOTjenestepensjonForhold: GBOTjenestepensjonForhold
+    ) {
+        GBOTjenestepensjonForhold.tssEksternId = OpprettTPForholdRequestInt.eksternTSSId // move (executionOrder=1)
+        GBOTjenestepensjonForhold.harUtlandPensjon = "false" // set (executionOrder=2)
+        val OpprettTPForholdRequestInt_extRequest_samtykke =
+            OpprettTPForholdRequestInt.extRequest.samtykke // custom.input.forEach (executionOrder=3)
+        GBOTjenestepensjonForhold.samtykkeSimuleringKode =
+            if ((OpprettTPForholdRequestInt_extRequest_samtykke != null && OpprettTPForholdRequestInt_extRequest_samtykke.value != null && OpprettTPForholdRequestInt_extRequest_samtykke.value!!)) "J" else "N" // custom.output assignment (executionOrder=3)
+        GBOTjenestepensjonForhold.harSimulering = "false" // set (executionOrder=4)
     }
 
-    public static void SlettTPForholdRequestIntTOGBOFinnTjenestepensjonsforholdRequest(SlettTPForholdFinnTjenestepensjonsforholdRequestInt SlettTPForholdRequestInt, nav_lib_frg.no.nav.lib.frg.gbo.GBOFinnTjenestepensjonsforholdRequest GBOFinnTjenestepensjonsforholdRequest) {
-        GBOFinnTjenestepensjonsforholdRequest.setTssEksternId(SlettTPForholdRequestInt.getEksternTSSId()); // move (executionOrder=1)
-        GBOFinnTjenestepensjonsforholdRequest.setFnr(SlettTPForholdRequestInt.getExtRequest().getFnr()); // move (executionOrder=2)
-        GBOFinnTjenestepensjonsforholdRequest.setHentSamhandlerInfo("false"); // set (executionOrder=3)
+    fun SlettTPForholdRequestIntTOGBOFinnTjenestepensjonsforholdRequest(
+        SlettTPForholdRequestInt: SlettTPForholdFinnTjenestepensjonsforholdRequestInt,
+        GBOFinnTjenestepensjonsforholdRequest: GBOFinnTjenestepensjonsforholdRequest
+    ) {
+        GBOFinnTjenestepensjonsforholdRequest.tssEksternId =
+            SlettTPForholdRequestInt.eksternTSSId // move (executionOrder=1)
+        GBOFinnTjenestepensjonsforholdRequest.fnr = SlettTPForholdRequestInt.extRequest.fnr // move (executionOrder=2)
+        GBOFinnTjenestepensjonsforholdRequest.hentSamhandlerInfo = "false" // set (executionOrder=3)
     }
 
-    public static void SlettTPForholdRequestIntTOGBOTjenestepensjonForhold(SlettTPForholdTjenestepensjonRequestInt SlettTPForholdTjenestepensjonRequestInt, nav_lib_frg.no.nav.lib.frg.gbo.GBOTjenestepensjonForhold GBOTjenestepensjonForhold) {
-        GBOTjenestepensjonForhold.setForholdId(SlettTPForholdTjenestepensjonRequestInt.getForholdId()); // move (executionOrder=1)
+    fun SlettTPForholdRequestIntTOGBOTjenestepensjonForhold(
+        SlettTPForholdTjenestepensjonRequestInt: SlettTPForholdTjenestepensjonRequestInt,
+        GBOTjenestepensjonForhold: GBOTjenestepensjonForhold
+    ) {
+        GBOTjenestepensjonForhold.forholdId =
+            SlettTPForholdTjenestepensjonRequestInt.forholdId // move (executionOrder=1)
     }
-
 }
