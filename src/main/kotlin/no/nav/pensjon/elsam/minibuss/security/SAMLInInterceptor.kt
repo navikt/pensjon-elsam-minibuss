5b50c6718d0f2d71dc9bbe1fa5f61abcfcd103a8
Per Christian Moen <per.christian.moen@nav.no>
2024-07-01 09:47:48 +0200

    Konvertert til Kotlin

--- src/main/kotlin/no/nav/pensjon/elsam/minibuss/security/SAMLInInterceptor.kt
+++ src/main/kotlin/no/nav/pensjon/elsam/minibuss/security/SAMLInInterceptor.kt
@@ -1,72 +1,67 @@
-package no.nav.pensjon.elsam.minibuss.security;
+package no.nav.pensjon.elsam.minibuss.security
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-
-import org.apache.cxf.binding.soap.SoapMessage;
-import org.apache.cxf.interceptor.Fault;
-import org.apache.cxf.security.SecurityContext;
-import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
-import org.apache.wss4j.common.principal.SAMLTokenPrincipal;
-import org.apache.wss4j.common.ext.WSSecurityException;
-import org.apache.wss4j.common.crypto.Crypto;
-import org.apache.wss4j.common.crypto.CryptoFactory;
-import org.apache.wss4j.dom.handler.RequestData;
-import org.opensaml.saml.saml2.core.Assertion;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import static no.nav.pensjon.elsam.minibuss.security.SAMLUtils.*;
-import static org.apache.wss4j.common.ConfigurationConstants.ACTION;
-import static org.apache.wss4j.common.ConfigurationConstants.SAML_TOKEN_SIGNED;
+import no.nav.pensjon.elsam.minibuss.security.SAMLUtils.samlAssertionToSubject
+import org.apache.cxf.binding.soap.SoapMessage
+import org.apache.cxf.interceptor.Fault
+import org.apache.cxf.security.SecurityContext
+import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor
+import org.apache.wss4j.common.ConfigurationConstants
+import org.apache.wss4j.common.ConfigurationConstants.ACTION
+import org.apache.wss4j.common.ConfigurationConstants.SAML_TOKEN_SIGNED
+import org.apache.wss4j.common.crypto.Crypto
+import org.apache.wss4j.common.crypto.CryptoFactory
+import org.apache.wss4j.common.ext.WSSecurityException
+import org.apache.wss4j.common.ext.WSSecurityException.ErrorCode.INVALID_SECURITY_TOKEN
+import org.apache.wss4j.common.principal.SAMLTokenPrincipal
+import org.apache.wss4j.dom.handler.RequestData
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.util.*
 
 /**
  * CXF Soap interceptor som validerer SAML-token og logger inn caller
  */
-public class SAMLInInterceptor extends WSS4JInInterceptor {
-    private static final Logger logger = LoggerFactory.getLogger(SAMLInInterceptor.class);
-
-    public SAMLInInterceptor(Map<String, Object> properties) {
-        super(new HashMap<>(properties));
-        setProperty(ACTION, SAML_TOKEN_SIGNED);
+class SAMLInInterceptor(properties: Map<String, Any>?) : WSS4JInInterceptor(HashMap(properties)) {
+    init {
+        setProperty(ACTION, SAML_TOKEN_SIGNED)
     }
 
-    @Override
-    public Crypto loadSignatureCrypto(RequestData requestData) throws WSSecurityException {
-        Properties signatureProperties = new Properties();
-        signatureProperties.setProperty("org.apache.wss4j.crypto.merlin.truststore.file", System.getProperty("javax.net.ssl.trustStore"));
-        signatureProperties.setProperty("org.apache.wss4j.crypto.merlin.truststore.password", System.getProperty("javax.net.ssl.trustStorePassword"));
+    override fun loadSignatureCrypto(requestData: RequestData): Crypto {
+        val signatureProperties = Properties()
+        signatureProperties.setProperty(
+            "org.apache.wss4j.crypto.merlin.truststore.file", System.getProperty("javax.net.ssl.trustStore")
+        )
+        signatureProperties.setProperty(
+            "org.apache.wss4j.crypto.merlin.truststore.password", System.getProperty("javax.net.ssl.trustStorePassword")
+        )
 
-        return CryptoFactory.getInstance(signatureProperties);
+        return CryptoFactory.getInstance(signatureProperties)
     }
 
-    @Override
-    public void handleMessage(SoapMessage msg) {
-        super.handleMessage(msg);
+    override fun handleMessage(msg: SoapMessage) {
+        super.handleMessage(msg)
 
-        SecurityContext sc = (SecurityContext) msg.get(SecurityContext.class.getName());
-        if(sc == null) {
-            throw new RuntimeException("Cannot get SecurityContext from SoapMessage");
-        }
-        SAMLTokenPrincipal samlTokenPrincipal = (SAMLTokenPrincipal) sc.getUserPrincipal();
-        if(samlTokenPrincipal == null) {
-            throw new RuntimeException("Cannot get SAMLTokenPrincipal from SecurityContext");
-        }
-        Assertion assertion = samlTokenPrincipal.getToken().getSaml2();
+        val sc = msg[SecurityContext::class.java.name] as SecurityContext?
+            ?: throw RuntimeException("Cannot get SecurityContext from SoapMessage")
+        val samlTokenPrincipal = sc.userPrincipal as SAMLTokenPrincipal?
+            ?: throw RuntimeException("Cannot get SAMLTokenPrincipal from SecurityContext")
+        val assertion = samlTokenPrincipal.token.saml2
 
-        if (logger.isDebugEnabled()) {
-            logger.debug("SAML Issuer: {}", assertion.getIssuer().getValue());
-            logger.debug("SAML Subject: {}", assertion.getSubject().getNameID().getValue());
+        if (logger.isDebugEnabled) {
+            logger.debug("SAML Issuer: {}", assertion.issuer.value)
+            logger.debug("SAML Subject: {}", assertion.subject.nameID.value)
         }
 
         try {
-            samlAssertionToSubject(assertion);
-        } catch (Exception e) {
-            logger.info("Login failed", e);
-            WSSecurityException wsSecurityException = new WSSecurityException(WSSecurityException.ErrorCode.INVALID_SECURITY_TOKEN, e);
-            throw new Fault(wsSecurityException, wsSecurityException.getFaultCode());
+            samlAssertionToSubject(assertion)
+        } catch (e: Exception) {
+            logger.info("Login failed", e)
+            val wsSecurityException = WSSecurityException(INVALID_SECURITY_TOKEN, e)
+            throw Fault(wsSecurityException, wsSecurityException.faultCode)
         }
     }
 
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(SAMLInInterceptor::class.java)
+    }
 }
--- src/main/kotlin/no/nav/pensjon/elsam/minibuss/security/SAMLUtils.kt
+++ src/main/kotlin/no/nav/pensjon/elsam/minibuss/security/SAMLUtils.kt
@@ -1,68 +1,58 @@
-package no.nav.pensjon.elsam.minibuss.security;
+package no.nav.pensjon.elsam.minibuss.security
 
-import org.opensaml.saml.saml2.core.Assertion;
-import org.opensaml.saml.saml2.core.Attribute;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.opensaml.saml.saml2.core.Assertion
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.StringWriter
+import javax.naming.InvalidNameException
+import javax.naming.ldap.LdapName
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
 
-import javax.naming.InvalidNameException;
-import javax.naming.ldap.LdapName;
-import javax.naming.ldap.Rdn;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+object SAMLUtils {
+    private val logger: Logger = LoggerFactory.getLogger(SAMLUtils::class.java)
 
-public class SAMLUtils {
-    private static final Logger logger = LoggerFactory.getLogger(SAMLUtils.class);
+    fun samlAssertionToSubject(assertion: Assertion) {
+        val uid = filterDNtoCNvalue(assertion.subject.nameID.value)
+        val attributes = assertion.attributeStatements[0].attributes
 
-    public static void samlAssertionToSubject(Assertion assertion) throws TransformerException {
-        final String uid = filterDNtoCNvalue(assertion.getSubject().getNameID().getValue());
-        final List<Attribute> attributes = assertion.getAttributeStatements().get(0).getAttributes();
+        val attributeMap: MutableMap<String, String> = HashMap()
+        for (attribute in attributes) {
+            val attributeName = attribute.name
+            val attributeValue = attribute.attributeValues[0].dom!!.firstChild.textContent
 
-        Map<String, String> attributeMap = new HashMap<>();
-        for (Attribute attribute : attributes) {
-            String attributeName = attribute.getName();
-            String attributeValue = attribute.getAttributeValues().get(0)
-                    .getDOM().getFirstChild().getTextContent();
-
-            attributeMap.put(attributeName, attributeValue);
+            attributeMap[attributeName] = attributeValue
         }
         if (uid == null) {
-            throw new RuntimeException("SAML assertion is missing mandatory element NameId");
+            throw RuntimeException("SAML assertion is missing mandatory element NameId")
         }
-        getSamlAssertionAsString(assertion);
+        getSamlAssertionAsString(assertion)
     }
 
-    static String getSamlAssertionAsString(Assertion assertion) throws TransformerException {
-        StringWriter writer = new StringWriter();
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-        transformer.transform(new DOMSource(assertion.getDOM()), new StreamResult(writer));
-        return writer.toString();
+    fun getSamlAssertionAsString(assertion: Assertion): String {
+        val writer = StringWriter()
+        val transformerFactory = TransformerFactory.newInstance()
+        val transformer = transformerFactory.newTransformer()
+        transformer.transform(DOMSource(assertion.dom), StreamResult(writer))
+        return writer.toString()
     }
 
-    public static String filterDNtoCNvalue(String userid) {
+    fun filterDNtoCNvalue(userid: String?): String? {
         try {
-            LdapName ldapname = new LdapName(userid);
-            String cn = null;
-            for (Rdn rdn : ldapname.getRdns()) {
-                if (rdn.getType().equalsIgnoreCase("CN")) {
-                    cn = rdn.getValue().toString();
-                    logger.debug("uid on DN form. Filtered from {} to {}", userid, cn);
-                    break;
+            val ldapname = LdapName(userid)
+            var cn: String? = null
+            for (rdn in ldapname.rdns) {
+                if (rdn.type.equals("CN", ignoreCase = true)) {
+                    cn = rdn.value.toString()
+                    logger.debug("uid on DN form. Filtered from {} to {}", userid, cn)
+                    break
                 }
             }
-            return cn;
-        } catch (InvalidNameException e) {
-            logger.debug("uid not on DN form. Skipping filter. {}", e.toString());
-            return userid;
+            return cn
+        } catch (e: InvalidNameException) {
+            logger.debug("uid not on DN form. Skipping filter. {}", e.toString())
+            return userid
         }
     }
-
 }
