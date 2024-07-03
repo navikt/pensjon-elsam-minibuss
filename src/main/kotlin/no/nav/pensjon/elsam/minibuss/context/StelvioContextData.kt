d890cb472fba2c1fdcabc931db30a683c864a276
Per Christian Moen <per.christian.moen@nav.no>
2024-07-03 13:48:24 +0200

    Legger på setter og languageId

--- src/main/kotlin/no/nav/pensjon/elsam/minibuss/context/StelvioContextData.kt
+++ src/main/kotlin/no/nav/pensjon/elsam/minibuss/context/StelvioContextData.kt
@@ -4,11 +4,14 @@ import jakarta.xml.bind.annotation.XmlElement
 import jakarta.xml.bind.annotation.XmlRootElement
 
 @XmlRootElement(name = "StelvioContext", namespace = "http://www.nav.no/StelvioContextPropagation")
-class StelvioContextData(
+@Suppress("HttpUrlsUsage")
+data class StelvioContextData(
     @get:XmlElement(name = "applicationId")
-    val applicationId: String? = null,
+    var applicationId: String? = null,
     @get:XmlElement(name = "correlationId")
     var correlationId: String? = null,
+    @get:XmlElement(name = "languageId")
+    var languageId: String? = null,
     @get:XmlElement(name = "userId")
-    val userId: String? = null,
+    var userId: String? = null,
 )
