679696f4eebbc594a804ff157c76274e060a1eeb
Per Christian Moen <per.christian.moen@nav.no>
2024-07-10 09:33:51 +0200

    Fjern ubrukt mapping

--- src/main/kotlin/no/nav/pensjon/elsam/minibuss/nav_cons_elsam_tptilb_registreretpforholdV0_1/RegistrereTPForholdIntTOTjenestepensjon.kt
+++ src/main/kotlin/no/nav/pensjon/elsam/minibuss/nav_cons_elsam_tptilb_registreretpforholdV0_1/RegistrereTPForholdIntTOTjenestepensjon.kt
@@ -1,6 +1,5 @@
 package no.nav.pensjon.elsam.minibuss.nav_cons_elsam_tptilb_registreretpforholdV0_1
 
-import nav_cons_elsam_tptilb_registreretpforhold.no.nav.asbo.HentTPForholdListeRequestInt
 import nav_cons_elsam_tptilb_registreretpforhold.no.nav.asbo.OpprettTPForholdRequestInt
 import nav_cons_elsam_tptilb_registreretpforhold.no.nav.asbo.SlettTPForholdFinnTjenestepensjonsforholdRequestInt
 import nav_cons_elsam_tptilb_registreretpforhold.no.nav.asbo.SlettTPForholdTjenestepensjonRequestInt
@@ -10,7 +9,6 @@ import nav_cons_sto_sam_tjenestepensjon.no.nav.inf.SAMTjenestepensjon
 import nav_lib_cons_sto_sam.no.nav.lib.sto.sam.asbo.tjenestepensjon.ASBOStoTjenestepensjon
 import nav_lib_frg.no.nav.lib.frg.fault.FaultElementetErDuplikat
 import nav_lib_frg.no.nav.lib.frg.inf.tjenestepensjon.*
-import no.nav.elsam.registreretpforhold.v0_1.HentTPForholdListeResp
 import org.springframework.stereotype.Component
 
 @Component
@@ -44,17 +42,6 @@ class RegistrereTPForholdIntTOTjenestepensjon(
             throw SlettTPForholdFinnTjenestepensjonsforholdIntFaultTjenestepensjonForholdIkkeFunnetIntMsg(e.message, e.faultInfo?.toFaultTjenestepensjonForholdIkkeFunnet())
         } catch (e: HentTjenestepensjonInfoFaultTomDatoForanFomDatoMsg) {
             throw SlettTPForholdFinnTjenestepensjonsforholdIntFaultGeneriskMsg(e.message, e.faultInfo?.toFaultGenerisk())
-        }
-    }
-
-    // RegistrereTPForholdIntTOFinnTjenestepensjonsforhold
-    fun hentTPForholdListeInt(hentTPForholdListeRequestInt: HentTPForholdListeRequestInt): HentTPForholdListeResp {
-        try {
-            return finnTjenestepensjonsforhold.finnTjenestepensjonsforhold(hentTPForholdListeRequestInt.toGBOFinnTjenestepensjonsforholdRequest()).toHentTPForholdListeResp()
-        } catch (e: HentTjenestepensjonInfoFaultElementetFinnesIkkeMsg) {
-            throw HentTPForholdListeIntFaultTjenestepensjonForholdIkkeFunnetMsg(e.message, e.faultInfo?.toFaultTjenestepensjonForholdIkkeFunnet())
-        } catch (e: HentTjenestepensjonInfoFaultTomDatoForanFomDatoMsg) {
-            throw HentTPForholdListeIntFaultGeneriskMsg(e.message, e.faultInfo?.toFaultGenerisk())
         }
     }
 
