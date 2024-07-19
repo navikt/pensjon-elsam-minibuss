f626ae9cc35603e6977b37c82fa6a6e13a6e048d
PålFleischer <pal.fleischer.borjeson@nav.no>
2024-07-19 13:15:26 +0200

    PES-221: opprettTPForhold, rest-kall til TP. Toggle med unleash i dev-fss. (#17)
    Co-authored-by: Jens Chr. Madsen <Jens.Christian.Madsen@nav.no>
    Co-authored-by: Mathias Sand Jahren <the.mather1@gmail.com>

--- src/main/kotlin/no/nav/pensjon/elsam/minibuss/tjenestepensjon/TjenestepensjonService.kt
+++ src/main/kotlin/no/nav/pensjon/elsam/minibuss/tjenestepensjon/TjenestepensjonService.kt
@@ -2,7 +2,11 @@ package no.nav.pensjon.elsam.minibuss.tjenestepensjon
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import no.nav.elsam.registreretpforhold.v0_1.OpprettTPForholdFaultGeneriskMsg
+import no.nav.elsam.registreretpforhold.v0_1.OpprettTPForholdFaultPersonIkkeFunnetMsg
 import org.ehcache.impl.internal.concurrent.ConcurrentHashMap
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.client.HttpClientErrorException.NotFound
 import org.springframework.web.client.RestClient
@@ -55,6 +59,21 @@ class TjenestepensjonService(
         return tjenestepensjon.forhold.isNotEmpty()
     }
 
+    fun opprettTPForhold(fnr: String, ordning: String)= tpRestClient.put()
+        .uri("/api/samhandler/tjenestepensjon/forhold/$ordning")
+        .body(SamhandlerForholdDto(kilde = "TPLEV", tpNr = ordning))
+        .header("fnr", fnr)
+        .exchange { _, clientResponse ->
+            when (clientResponse.statusCode) {
+                HttpStatus.OK -> Unit
+                HttpStatus.CONFLICT -> Unit
+                HttpStatus.NOT_FOUND -> throw OpprettTPForholdFaultPersonIkkeFunnetMsg(clientResponse.bodyTo(String::class.java))
+                else -> {
+                    throw OpprettTPForholdFaultGeneriskMsg(clientResponse.bodyTo(String::class.java))
+                }
+            }
+        }
+
     @JsonIgnoreProperties(ignoreUnknown = true)
     data class OrdningDto(
         val navn: String,
@@ -90,5 +109,49 @@ class TjenestepensjonService(
     data class CachedValue<T>(
         val value: T,
         val fetchTime: LocalDateTime = LocalDateTime.now(),
+    )
+
+    interface TjenestepensjonForhold
+
+    //hoved forholdDto
+    class SamhandlerForholdDto(
+        val kilde: String, //KildeTypeCode
+        tpNr: String,
+        val ytelser: List<SamhandlerYtelseDto> = emptyList(),
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+        datoSistOpptjening: LocalDate? = null,
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+        sistEndretDatoSistOpptjening: LocalDateTime? = null,
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+        val changeStamp: ChangeStampDateDto? = null,
+        harGjenlevendeYtelse: Boolean? = null
+    ) : SamhandlerForholdsinfoDto(tpNr, "", datoSistOpptjening, sistEndretDatoSistOpptjening, harGjenlevendeYtelse)
+
+    //også kjent som forholdListe
+    open class SamhandlerForholdsinfoDto(
+        val tpNr: String,
+        val tpOrdningNavn: String,
+        val datoSistOpptjening: LocalDate? = null,
+        val sistEndretDatoSistOpptjening: LocalDateTime? = null,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        val harGjenlevendeYtelse: Boolean? = false
+    ): TjenestepensjonForhold
+
+    data class ChangeStampDateDto(
+        val createdBy: String,
+        val createdDate: LocalDateTime,
+        val updatedBy: String,
+        val updatedDate: LocalDateTime
+    )
+
+    data class SamhandlerYtelseDto(
+        val datoInnmeldtYtelseFom: LocalDate? = null,
+        val ytelseType: String, //YtelseTypeCode
+        val datoYtelseIverksattFom: LocalDate? = null,
+        val datoYtelseIverksattTom: LocalDate? = null,
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        val changeStamp: ChangeStampDateDto? = null,
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        val ytelseId: Long? = null
     )
 }
