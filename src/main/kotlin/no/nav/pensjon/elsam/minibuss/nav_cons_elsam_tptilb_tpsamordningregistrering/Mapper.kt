0f06f310a2df5e899a32c075e2c23aaf2de2e153
Morten Normann <morten.normann@nav.no>
2024-10-14 11:33:10 +0200

    Revert "Merge pull request #45 from navikt/revert/slett-tp-ytelse"
    This reverts commit 2b7b5cc57ce17827064b3d07b173d754e741391f, reversing
    changes made to 6d6b18caa62ec551692260da717a8b814fd6fe5b.

--- src/main/kotlin/no/nav/pensjon/elsam/minibuss/nav_cons_elsam_tptilb_tpsamordningregistrering/Mapper.kt
+++ src/main/kotlin/no/nav/pensjon/elsam/minibuss/nav_cons_elsam_tptilb_tpsamordningregistrering/Mapper.kt
@@ -1,8 +1,6 @@
 package no.nav.pensjon.elsam.minibuss.nav_cons_elsam_tptilb_tpsamordningregistrering
 
 import nav_cons_elsam_tptilb_tpsamordningregistrering.no.nav.asbo.OpprettRefusjonskravReqInt
-import nav_cons_elsam_tptilb_tpsamordningregistrering.no.nav.asbo.SlettTPYtelseReqInt
-import nav_lib_frg.no.nav.lib.frg.fault.FaultPersonIkkeFunnet
 import nav_lib_sto.no.nav.lib.sto.fault.*
 import nav_lib_sto.no.nav.lib.sto.gbo.*
 import no.nav.elsam.tpsamordningregistrering.v0_5.*
@@ -11,22 +9,6 @@ import javax.xml.datatype.DatatypeFactory
 import javax.xml.datatype.XMLGregorianCalendar
 
 object Mapper {
-    // FaultKombinasjonInputTOFaultTPYtelseIkkeFunnet.map
-    fun FaultKombinasjonInput.toFaultTPYtelseIkkeFunnet() =
-        FaultTPYtelseIkkeFunnet().also {
-            it.errorMessage = errorMessage // move (executionOrder=1)
-            it.errorSource = errorSource // move (executionOrder=2)
-            it.rootCause = rootCause // move (executionOrder=3)
-            it.dateTimeStamp = dateTimeStamp?.toXMLGregorianCalendar() // move (executionOrder=4)
-        }
-
-    // FaultPersonIkkeFunnetTOFaultGenerisk.map
-    fun FaultPersonIkkeFunnet.toFaultGenerisk() =
-        FaultGenerisk().also {
-            it.errorCode = "UnknownId" // set (executionOrder=1)
-            it.errorDescription = errorMessage // move (executionOrder=2)
-        }
-
     // FaultRefKravAlleredeRegElUtenforFristTOFaultAlleredeMottattRefusjonskrav.map
     fun FaultRefKravAlleredeRegElUtenforFrist.toFaultAlleredeMottattRefusjonskrav() =
         FaultAlleredeMottattRefusjonskrav().also {
@@ -72,18 +54,6 @@ object Mapper {
             it.kravstillersRef = kravstillersReferanse // move (executionOrder=2)
             it.datoFom = datoFom?.let { x -> formatWIDString(x.toDate()) } // custom.output assignment (executionOrder=3)
             it.datoTom = datoTom?.let { x -> formatWIDString(x.toDate()) } // custom.output assignment (executionOrder=4)
-        }
-
-    // SlettTPYtelseReqIntTOGBOSlettTPSamordningRequest.map
-    fun SlettTPYtelseReqInt.toGBOSlettTPSamordningRequest() =
-        GBOSlettTPSamordningRequest().also {
-            it.tpnr = extRequest.tpnr // move (executionOrder=1)
-            it.fnr = extRequest.fnr // move (executionOrder=2)
-            it.meldingKode = extRequest.henvendelseType // move (executionOrder=3)
-            it.iverksattFom = extRequest.datoFom?.toDate()?.let { x -> formatWIDString(x) } // custom.output assignment (executionOrder=4)
-            it.iverksattTom = extRequest.datoTom?.toDate()?.let { x -> formatWIDString(x) } // custom.output assignment (executionOrder=5)
-            it.tssEksternId = tssEksternId // move (executionOrder=6)
-            it.ytelseKode = extRequest.tpArt // move (executionOrder=7)
         }
 
     private fun String.toXMLGregorianCalendar() = DatatypeFactory.newInstance().newXMLGregorianCalendar(this)
--- src/main/kotlin/no/nav/pensjon/elsam/minibuss/sam/SamService.kt
+++ src/main/kotlin/no/nav/pensjon/elsam/minibuss/sam/SamService.kt
@@ -1,10 +1,19 @@
 package no.nav.pensjon.elsam.minibuss.sam
 
+import no.nav.elsam.tpsamordningregistrering.v0_5.SlettTPYtelseReq
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
 
 @Service
 class SamService(
     private val samRestClient: RestClient
 ) {
+    fun slettTPYtelse(request: SlettTPYtelseReq) {
+        samRestClient.post()
+            .uri("/api/tp/ytelse/slett")
+            .body(request)
+            .retrieve()
+            .body<String>()
+    }
 }
