7df90c8f0bb787f886c5ec245336b144309022ea
Morten Normann <morten.normann@nav.no>
2024-10-14 10:42:06 +0200

    Revert "Merge pull request #42 from navikt/slett-tp-ytelse"
    This reverts commit 6d6b18caa62ec551692260da717a8b814fd6fe5b, reversing
    changes made to 82030f203997c22173278d37a20a846be411b030.

--- src/main/kotlin/no/nav/pensjon/elsam/minibuss/sam/SamService.kt
+++ src/main/kotlin/no/nav/pensjon/elsam/minibuss/sam/SamService.kt
@@ -1,19 +1,10 @@
 package no.nav.pensjon.elsam.minibuss.sam
 
-import no.nav.elsam.tpsamordningregistrering.v0_5.SlettTPYtelseReq
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
-import org.springframework.web.client.body
 
 @Service
 class SamService(
     private val samRestClient: RestClient
 ) {
-    fun slettTPYtelse(request: SlettTPYtelseReq) {
-        samRestClient.post()
-            .uri("/api/tp/ytelse/slett")
-            .body(request)
-            .retrieve()
-            .body<String>()
-    }
 }
